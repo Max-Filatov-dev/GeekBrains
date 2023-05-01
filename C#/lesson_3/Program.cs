ca60bc1569b4500b3dc26f1a4644967a48254e50
Max_Filatov <maxsthm@mail.ru>
2023-05-01 12:45:06 +0300

    Added method GetDiastance and TAbleCube

--- C#/lesson_3/Program.cs
+++ C#/lesson_3/Program.cs
@@ -4,26 +4,79 @@
 
 bool PolindromNumber()
 {
-    int num, rem, sum = 0, temp;
+    int number, remineder, temp, total = 0;
     Console.Write("Enter a number: ");
-    num = Convert.ToInt32(Console.ReadLine());
-    temp = num;
-    while (num > 0)
+    number = Convert.ToInt32(Console.ReadLine());
+    temp = number;
+    while (number > 0)
     {
-        rem = num % 10;
-        Console.WriteLine($"rem: {rem}");
-        num = num / 10;
-        Console.WriteLine($"num: {num}");
-        sum = sum * 10 + rem;
-        Console.WriteLine($"sum: {sum}");
+        remineder = number % 10;
+        number /= 10;
+        total = total * 10 + remineder;
     }
-    if (temp == sum)
+    if (temp == total)
         return true;
     else
         return false;
 }
-
+/*
 if (PolindromNumber())
     Console.WriteLine("\nNumber is Palindrome\n");
 else
     Console.WriteLine("\nNumber is not a palindrome\n");
+*/
+
+
+
+// Task 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return distance;
+}
+
+// Тип var -> неизвестно, что введет пользователь. Возможно int (5), возможно double (5,37)
+/*
+Console.Write("Введите координаты x1: ");
+var firstX = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y1: ");
+var firstY = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z1: ");
+var firstZ = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты x2: ");
+var secondX = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты y2: ");
+var secondY = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты z2: ");
+var secondZ = Convert.ToDouble(Console.ReadLine());
+
+double response = GetDistance(x1: firstX, y1: firstY, z1: firstZ, x2: secondX, y2: secondY, z2: secondZ);
+Console.WriteLine($"Distance between two points = {Math.Round(response, 2)}");
+*/
+
+
+
+// Task 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void TableCube(int number)
+{
+    for (int i = 0; i <= number; i++)
+    {
+        for (int j = 1; j <= number; j++)
+        {
+            double numberCube = Math.Pow(j, 3);
+            Console.Write($"{numberCube}, ");
+        }
+        Console.WriteLine();
+    }
+}
+/*
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+TableCube(number: number);
+*/
