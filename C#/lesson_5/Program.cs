ad58d0cad279a69a66e88986aa3b6e5274754976
Max_Filatov <maxsthm@mail.ru>
2023-05-02 15:00:46 +0300

    C# lesson_5

---
+++ C#/lesson_5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task 34
+
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 500);
+    Console.Write($"{array[i]} ");
+}
+
+int QuantityEvenNumber()
+{
+    int countEven = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) countEven++;
+    return countEven;
+}
+
+Console.WriteLine($"\n\nQuantity of even numbers: {QuantityEvenNumber()}\n");
