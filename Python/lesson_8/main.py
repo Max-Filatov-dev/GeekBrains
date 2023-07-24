f49be45559d4269f4b3ba20d527c5027e524c794
Max_Filatov <maxsthm@mail.ru>
2023-07-24 09:53:44 +0300

    lesson_ 8

--- Python/lesson_8/main.py
+++ Python/lesson_8/main.py
@@ -4,9 +4,10 @@ import json
 Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных. Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
 """
 
-main_dict = {'menu': ('1. Открыть файл', '2. Показать контакты', '3. Добавить контакт',
-                      '4. Найти контакт', '5. Изменить контакт', '6. Удалить контакт', '7. Сохранить файл', '8. Выход'),
-             'contacts': {1: ('Филатов Максим', 123, 'Software'), 2: ('Морозов Евгений', 456, 'Engeenies')}}
+main_dict = {'menu': ('1. Открыть файл', '2. Показать контакты', '3. Найти контакт','4. Добавить контакт',
+                      '5. Изменить контакт', '6. Удалить контакт', '7. Сохранить файл', '8. Вернуть файл к шаблону', '9. Выход'),
+             'contacts': [(1, 'Филатов Максим', 123, 'Software'), (2, 'Морозов Евгений', 456, 'Engeenies')]}
+
 
 def check_data() -> str:
     while True:
@@ -17,7 +18,7 @@ def check_data() -> str:
         if option.isdigit():
             return option
         else:
-            print(f'\n{"-"*45}\nНеобходимо ввести номер действия (1, 2...)\n{"-"*45}\n')
+            print(f'\n{"-"*45}\nВведите Номер (1, 2...)\n{"-"*45}\n')
 
 
 def open_file():
@@ -27,19 +28,43 @@ def open_file():
     return data
 
 
-def save_file():
+def new_contact():
+    """ """
+    name_fio = input('\nВведите ФИО: ')
+    phone = input('Введите телефон: ')
+    comment = input('Введите комментарий: ')
+    return name_fio, phone, comment
+
+
+def change_contact(contacts: list):
+    """ """
+    change_name = input('\nВведите имя: ').lower()
+    select = [k for k in contacts if change_name in k[1].lower()]
+    if select:
+        print(f'{"-"*30}\n{select[0]}')
+        resp = select[0][0], *new_contact()
+        contacts[resp[0]-1] = resp
+        return contacts
+    else:
+        print(f'{"-"*30}\nНет таких контактов!')
+
+    
+def del_contact(del_cont: list):
+    """ """
+    del_name = input('\nВведите имя: ').lower()
+    del_serch = [k for k in del_cont if del_name in k[1].lower()]
+    if del_serch:
+        del_cont.pop(del_serch[0][0]-1)
+        print(f'Контак удален: {del_serch[0]}')
+        return del_cont
+    else:
+        print(f'{"-"*30}\nНет таких контактов!')
+
+
+def save_file(save_data: dict):
     """ """
     with open('lesson_8/phone_book.json', 'w', encoding='utf-8') as pb:
-        json.dump(main_dict, pb, indent=4, ensure_ascii=False)
-
-
-def new_contact(person: list):
-    """ """
-    print(person)
-    # main_dict['contacts'].update({
-    #     3: person
-    # }
-    # )
+        json.dump(save_data, pb, indent=4, ensure_ascii=False)
 
 
 def main():
@@ -51,26 +76,55 @@ def main():
         match response:
             case 1:
                 tmp_data = open_file()
-                print(f'\n{"-"*30}\nФайл успешно открыт.\n{"-"*30}\n') if tmp_data else print(f'\n{"-"*30}\nError open!')
+                print(f'\n{"-"*30}\nФайл успешно открыт.\n{"-"*30}\n') if tmp_data else print(f'\n{"-"*30}\nАбшибка открытия!')
             case 2:
                 if tmp_data:
-                    contacts = [k for k in tmp_data['contacts'].values()]
-                    print(f'\n{"-"*30}\nКонтакты:', *contacts, sep='\n\t')
+                    contacts = [k for k in tmp_data['contacts']]
+                    print(f'\nПоказать контакты\n{"-"*30}', *contacts, sep='\n\t')
                 else:
                     print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
             case 3:
-                new_pers = []
-                name_fio = input('\nВведите ФИО: ')
-                phone = input('Введите телефон: ')
-                comment = input('Введите комментарий: ')
-                new_pers.extend((name_fio, phone, comment)) if name_fio and phone and comment else None
-                new_contact(person=new_pers) if new_pers else print(f'\n{"-"*30}\nВведены не все данные!\n{"-"*30}\n')
+                if tmp_data:
+                    search_name = input(f'\nНайти контакт\n{"-"*20}\nВведите имя: ').lower()
+                    result_search = [k for k in tmp_data['contacts'] if search_name in k[1].lower()]
+                    print(f'{"-"*30}\n{result_search}') if result_search else print(f'{"-"*30}\nНет таких контактов!')
+                else:
+                    print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
+            case 4:
+                if tmp_data:
+                    resp_new = new_contact()
+                    next_number = max([i[0] for i in tmp_data['contacts']])+1
+                    tmp_data['contacts'].append([next_number, *resp_new]) if resp_new else None
+                    print(f'{"-"*30}\nДобавлен контакт: {next_number} {resp_new}\n')
+                else:
+                    print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
+            case 5:
+                if tmp_data:
+                    resp_change = change_contact(contacts=tmp_data['contacts'])
+                    tmp_data['contacts'] = resp_change if resp_change else tmp_data['contacts']
+                else:
+                    print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
+            case 6:
+                if tmp_data:
+                    resp_del = del_contact(del_cont=tmp_data['contacts'])
+                    tmp_data['contacts'] = resp_del if resp_del else tmp_data['contacts']
+                else:
+                    print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
+            case 7:
+                save_file(save_data=tmp_data) if tmp_data else print(f'\n{"-"*30}\nНеобходимо открыть файл!\n{"-"*30}\n')
             case 8:
+                sogl = input('Уверены что хотите перезаписать файл? (y/n) ')
+                if sogl == 'y':
+                    save_file(save_data=main_dict)
+                    print(f'\n{"-"*20}\nФайл перезаписан!\n{"-"*20}\n')
+                    break
+            case 9:
                 print(f'\n{"-"*10}\nВыход.\n{"-"*10}\n')
                 break
-
-    # save_file()
+            case _:
+                print(f'\n{"-"*25}\nНе корректный выбор действия!\n{"-"*25}\n')
 
 
 if __name__ == '__main__':
     main()
+    # save_file()
