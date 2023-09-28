ff96b790aff2730fc75e3a60a6cddbcd2ef05b51
Max_Filatov <maxsthm@mail.ru>
2023-09-28 18:52:00 +0300

    Java exceptions_2

---
+++ Java_ exceptions/java_exceptions_2/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+//import main.lesson2.Lesson2;
+import main.homework.HomeWork;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Lesson2 lesson2 = new Lesson2();
+//        lesson2.wordVerification();
+
+        HomeWork homeWork = new HomeWork();
+
+        // Homework task 1
+        System.out.println("Lesson 2 Task 1");
+        System.out.println("Вы ввели: " + homeWork.inputNumber());
+
+        // Homework task 2
+        System.out.println("\nLesson 2 Task 2");
+        homeWork.correctingCode();
+
+        // Homework task 3
+        System.out.println("\nLesson 2 Task 3");
+        homeWork.correctingCodeSecond();
+
+        // Homework task 4
+        System.out.println("\nLesson 2 Task 4");
+        homeWork.emptyLine();
+
+
+    }
+
+
+}
+
